b91ad2ea51887736289dbb73cf639b1272093023
caiphus <caiphus@thedigitalacademy.co.za>
2022-10-31 09:51:36 +0200

    contact done

--- frontend/src/pages/contact/contact.jsx
+++ frontend/src/pages/contact/contact.jsx
@@ -1,46 +1,22 @@
 import "./contact.css";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import { FaEnvelope, FaLocationArrow, FaPhone } from "react-icons/fa";
-import { MDBInput,MDBInputGroup, MDBBtn,MDBTextArea } from 'mdb-react-ui-kit';
 
 function Contact() {
   return (
     <div id="contact">
-      <Container>
-
-     
-        <Col>
-          <Row className="justify-content-center">
-            <h1 className="justify-content-center">Get in Touch</h1>
-          </Row>
-
-          <Row className="justify-content-center">
-            <Col>
-              {" "}
-              <MDBInput label='Email' id='form1' type='text' />
-              <FaEnvelope color="primary" />{" "}
-            </Col>
-          
-          </Row>
-
-          <Row> 
-            
-
-          <MDBInputGroup>
-              <MDBTextArea label='Message' id='textAreaExample' rows={12} col={12} width={6}/>
-          </MDBInputGroup>
-
-          </Row>
-
-          <Row>
-            <MDBBtn className='me-1' color='success'>Button</MDBBtn>
-          </Row>
-
-       
-        </Col>
-      </Container>
+      <div className="header">
+        Contact <span className="pink">Us</span>
+      </div>
+      <div className="slogan">
+        <h1>Don't be shy.</h1>
+        <h1>
+          Make the first <span className="pink">contact.</span>
+        </h1>
+      </div>
+      <div className="contact">
+        <input type="email" placeholder="Email..." />
+        <textarea placeholder="Message..."></textarea>
+        <button>Send</button>
+      </div>
     </div>
   );
 }
